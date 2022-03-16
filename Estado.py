ecb6a0fa93221974bd842dfc13fa0df0a7921a71
saritazavala <saritazg2010@gmail.com>
2022-03-15 20:30:06 -0600

    Hago una clase para las transiciones? o lo hago de otra forma? thinking

--- Estado.py
+++ Estado.py
@@ -2,3 +2,9 @@ class Estado:
     def __init__(self estado_inicial, estado_final):
         self.estado_inicial = estado_inicial
         self.estado_final = estado_final
+
+class Transicion:
+    def __init__(self, punto_inicio, transicion, final):
+        self.punto_inicio = punto_inicio
+        self.transicion = transicion
+        self.final = final
