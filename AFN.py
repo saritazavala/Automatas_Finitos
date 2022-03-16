e42715256230fc8b64be4b563c568df0ee593537
saritazavala <saritazg2010@gmail.com>
2022-03-15 20:22:18 -0600

    Clase automata con estado ha sido creada

---
+++ AFN.py
@@ -0,0 +1,14 @@
+
+class AFN:
+    def __init__(self, estados, expresion, alfabeto, estado_inicial, estado_final, transicion ):
+        self.estados = estados
+        self.expresion = expresion
+        self.alfabeto = alfabeto
+        self.estado_inicial = estado_inicial
+        self.estado_final = estado_final
+        self.transicion = transicion
+
+        
+
+
+
---
+++ Estado.py
@@ -0,0 +1,4 @@
+class Estado:
+    def __init__(self estado_inicial, estado_final):
+        self.estado_inicial = estado_inicial
+        self.estado_final = estado_final
